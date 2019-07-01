62b6d3deca5334391a925bb63bd83230ed66daab
Magnus Edenhill <magnus@edenhill.se>
2019-07-01 13:02:06 +0200

    Show-casing the statically bundled confluent-kafka-go-dev client

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # confluent-kafka-go-example
-Example application using the confluent-kafka-go client
+
+Example application using the confluent-kafka-go client.
+
+This example is show-casing the experimental confluent-kafka-go-dev client
+that bundles all required dependencies (librdkafka with friends),
+allowing the client to be built and deployed without having to install
+librdkafka first.
+
+
+## How to build and run
+
+On OSX or glibc-based Linux (centos, ubuntu, ..):
+
+    $ go run main.go mybroker mytopic
+
+On Alpine:
+
+    $ go run -tags musl main.go mybroker mytopic
+
---
+++ main.go
@@ -0,0 +1,123 @@
+// Example application using confluent-kafka-go-dev (static builds)
+package main
+
+/**
+ * Copyright 2019 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/confluentinc/confluent-kafka-go-dev/kafka"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+func main() {
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	if len(os.Args) != 3 {
+		_, libver := kafka.LibraryVersion()
+		fmt.Fprintf(os.Stderr,
+			"Example application (librdkafka v%s, linkage %s)\n",
+			libver, kafka.LibrdkafkaLinkInfo)
+
+		fmt.Fprintf(os.Stderr, "Usage: %s <broker> <topic>\n",
+			os.Args[0])
+		os.Exit(1)
+	}
+
+	broker := os.Args[1]
+	topic := os.Args[2]
+
+	conf := kafka.ConfigMap{"bootstrap.servers": broker}
+
+	p, err := kafka.NewProducer(&conf)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+
+	tp := kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny}
+
+	// Go-routine for handling delivery reports
+	go func(drs chan kafka.Event) {
+		for ev := range drs {
+			m, ok := ev.(*kafka.Message)
+			if !ok {
+				continue
+			}
+			if m.TopicPartition.Error != nil {
+				fmt.Fprintf(os.Stderr, "%% Delivery error: %v\n", m.TopicPartition)
+			} else {
+				fmt.Fprintf(os.Stderr, "%% Delivered %v\n", m)
+			}
+		}
+	}(p.Events())
+
+	fmt.Fprintf(os.Stderr, "%% Enter message to produce and hit enter\n")
+	fmt.Fprintf(os.Stderr, "%% Ctrl-D to exit to flush messages and exit\n")
+
+	reader := bufio.NewReader(os.Stdin)
+	stdinChan := make(chan string)
+
+	go func() {
+		for true {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				break
+			}
+
+			line = strings.TrimSuffix(line, "\n")
+			if len(line) == 0 {
+				continue
+			}
+
+			stdinChan <- line
+		}
+		close(stdinChan)
+	}()
+
+	run := true
+
+	for run == true {
+		select {
+		case sig := <-sigs:
+			fmt.Fprintf(os.Stderr, "%% Terminating on signal %v\n", sig)
+			run = false
+
+		case line, ok := <-stdinChan:
+			if !ok {
+				run = false
+				break
+			}
+
+			msg := kafka.Message{
+				TopicPartition: tp,
+				Value:          ([]byte)(line),
+			}
+
+			p.ProduceChannel() <- &msg
+		}
+	}
+
+	fmt.Fprintf(os.Stderr, "%% Flushing %d message(s)\n", p.Len())
+	p.Flush(10000)
+	fmt.Fprintf(os.Stderr, "%% Closing\n")
+	p.Close()
+}
